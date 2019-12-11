b3c0b1212b71ae409464c2e6190f80c6d5fef128
kreideroliver <oliver.kreider@web.de>
2019-12-11 15:22:02 +0100

    Challenges auf Startseite; Activity für Grafik und Eintragen erstellt

---
+++ app/src/main/java/thu/change/GraphicActivity.java
@@ -0,0 +1,14 @@
+package thu.change;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GraphicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic);
+    }
+}
